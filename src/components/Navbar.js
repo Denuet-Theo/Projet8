62e1a35dabf99902d3780b2e360e7214835e55d4
denue <denuettheo@gmail.com>
2023-07-20 18:43:09 +0200

    modif navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,17 @@ function NavBar() {
   return (
     <>
       <header className="bg-grey">
-        <nav className="sm:px-12 sm:py-8 flex sm:justify-end justify-center">
-          <div className="sm:flex grid grid-cols-2 gap-x-[70%] sm:gap-x-0">
+        <nav className="sm:px-12 sm:py-8 sm:flex sm:justify-end grid grid-cols-2">
+          <div className="m-auto sm:m-0">
             <Link href="/Accueil" className="btn btn-ghost rounded-btn">Accueil</Link>
+          </div>
+          <div className="m-auto sm:m-0">
             <Link href="/works" className="btn btn-ghost rounded-btn">Travaux</Link>
+          </div>
+          <div className="m-auto sm:m-0">
             <Link href="/about" className="btn btn-ghost rounded-btn">A propos</Link>
+          </div>
+          <div className="m-auto sm:m-0">
             <Link href="/Admin/LoginPage" className="btn btn-ghost rounded-btn">Connexion</Link>
           </div>
         </nav>
