ad1031a5ab9e2cb9cf68e9df84f6299a1f2aaefa
denue <denuettheo@gmail.com>
2023-07-24 15:43:35 +0200

    debug filereader

--- src/components/Pdf/GenerateCvPdf.js
+++ src/components/Pdf/GenerateCvPdf.js
@@ -1,23 +1,27 @@
 import jsPDF from 'jspdf'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function GeneratePDF() {
     const [imageData64, setImageData64] = useState()
+
+    useEffect(() => {
+
+      const getBase64FromUrl = async (url) => {
+          const data = await fetch(url);
+          const blob = await data.blob();
+          return new Promise((resolve) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(blob); 
+            reader.onloadend = () => {
+              const base64data = reader.result;   
+              //resolve(base64data);
+              setImageData64(base64data)
+            }
+          });
+        }
+        getBase64FromUrl("https://cdn-images.zety.fr/pages/modeles_cv_word_gratuits_modernes_10.png").then(console.log)
+    },[imageData64])
    
-    const getBase64FromUrl = async (url) => {
-        const data = await fetch(url);
-        const blob = await data.blob();
-        return new Promise((resolve) => {
-          const reader = new FileReader();
-          reader.readAsDataURL(blob); 
-          reader.onloadend = () => {
-            const base64data = reader.result;   
-            //resolve(base64data);
-            setImageData64(base64data)
-          }
-        });
-      }
-      getBase64FromUrl("https://cdn-images.zety.fr/pages/modeles_cv_word_gratuits_modernes_10.png").then(console.log)
       
         function generate(){
         const doc= new jsPDF('portrait', 'px', 'a4',false)
